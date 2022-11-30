e229604399d0cf2f508954a9a25b4b716460f612
pidanmeng <1092333914@qq.com>
2022-11-30 15:01:03 +0800

    feat: upload release

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('initialization: pre-push');
+var core = require('@actions/core');
+
+function _interopNamespaceDefault(e) {
+	var n = Object.create(null);
+	if (e) {
+		Object.keys(e).forEach(function (k) {
+			if (k !== 'default') {
+				var d = Object.getOwnPropertyDescriptor(e, k);
+				Object.defineProperty(n, k, d.get ? d : {
+					enumerable: true,
+					get: function () { return e[k]; }
+				});
+			}
+		});
+	}
+	n.default = e;
+	return Object.freeze(n);
+}
+
+var core__namespace = /*#__PURE__*/_interopNamespaceDefault(core);
+
+const username = core__namespace.getInput('username');
+console.log(`[force-sync-gitee-action]: Your username is ${username}`);
