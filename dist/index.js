777561c8cbb4b3a250469df7aa2da56ead6cc265
pidanmeng <1092333914@qq.com>
2022-11-30 15:29:02 +0800

    refactor: 💡 use tsc to build

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,3 @@
-'use strict';
-
-var core = require('@actions/core');
-
-function _interopNamespaceDefault(e) {
-	var n = Object.create(null);
-	if (e) {
-		Object.keys(e).forEach(function (k) {
-			if (k !== 'default') {
-				var d = Object.getOwnPropertyDescriptor(e, k);
-				Object.defineProperty(n, k, d.get ? d : {
-					enumerable: true,
-					get: function () { return e[k]; }
-				});
-			}
-		});
-	}
-	n.default = e;
-	return Object.freeze(n);
-}
-
-var core__namespace = /*#__PURE__*/_interopNamespaceDefault(core);
-
-const username = core__namespace.getInput('username');
-core__namespace.info(`[force-sync-gitee-action]: Your username is ${username}`);
+import * as core from '@actions/core';
+const username = core.getInput('username');
+core.info(`[force-sync-gitee-action]: Your username is ${username}`);
