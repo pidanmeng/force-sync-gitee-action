87e57c9a5a7d01af03c080a8f51d28e977532901
pidanmeng <1092333914@qq.com>
2022-11-30 15:00:53 +0800

    refactor: 💡 init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('initialization: pre-push');
+import * as core from '@actions/core';
+
+const username = core.getInput('username');
+console.log(`[force-sync-gitee-action]: Your username is ${username}`);
